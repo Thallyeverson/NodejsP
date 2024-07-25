fc3867836ccdc9e433089a84c96014d61c2ed635
Thallyeverson <70922087+Thallyeverson@users.noreply.github.com>
2024-07-25 23:19:49 +0000

    0.2

--- biblioteca/routes/index.js
+++ biblioteca/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Projeto de Programação WEB' });
 });
 
+router.get('/TESTE', function(req, res, next) {
+  res.send("<h2>TESTANDO 1...2...3 </h2>");
+});
+
+router.get('/Boanoite/:nome', function(req, res, next) {
+res.send("<h2>Boa noite "+ req.params.nome +"!</h2>");
+});
+
+router.get('/imc', function(req, res,) {
+  var peso = req.query.peso;
+  var estatura = req.query.estatura;
+
+  var imc = peso / Math.pow(estatura, 2);
+  var msg = "<h3> Seu IMC é: " + imc.toFixed(2) + "</h3>";
+  res.send(msg);
+});
+
 module.exports = router;
